878651e435928cd0fdfafc786da8bf410f84128f
Mihai <veres85@yahoo.com>
2021-10-07 17:27:36 +0000

    Push from lets-go

--- theme/src/index.ts
+++ theme/src/index.ts
@@ -5,7 +5,5 @@ import { createTheme as createThemeMui, PaletteMode } from '@mui/material';
 export const createTheme = (mode: PaletteMode) =>
   createThemeMui({
     palette: mode === 'light' ? light : dark,
-    // roundness,
-    // fonts: configureFonts(fontConfig),
-    // animation,
+    typography,
   });
