a65cc5d73ef1c09f52e8f97a2d6776ef302f8322
mveres <veres85@yahoo.com>
2021-10-07 23:27:10 +0300

    first generated comp

--- autocomplete/src/autocomplete.ts
+++ autocomplete/src/autocomplete.ts
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export { default as Autocomplete } from '@mui/material/Autocomplete';
+=======
+export { Autocomplete } from '@mui/material/Autocomplete';
+>>>>>>> 8b18869 (first generated comp)
