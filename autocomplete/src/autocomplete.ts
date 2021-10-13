98f3a13736b1ee5eccb02da9c149b2053f2887fb
mveres <veres85@yahoo.com>
2021-10-13 10:39:30 +0300

    some demo components

--- autocomplete/src/autocomplete.ts
+++ autocomplete/src/autocomplete.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export { default as Autocomplete } from '@mui/material/Autocomplete';
-=======
-export { Autocomplete } from '@mui/material/Autocomplete';
->>>>>>> 8b18869 (first generated comp)
